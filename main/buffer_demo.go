68d30d90ebf5de25ce6afe9a9dbd7c06c90dd2fd
liuhang <liu.hang@netstars.co.jp>
2020-10-22 00:34:11 +0900

    bytes.Buffer learned

---
+++ main/buffer_demo.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	var buffer1 bytes.Buffer
+	contents := "Simple byte buffer for marshaling data."
+	fmt.Printf("Waiting contents %q ....\n", contents)
+	buffer1.WriteString(contents)
+	fmt.Printf("The length of buffer :%d\n", buffer1.Len())
+	fmt.Printf("The capcity of buffer :%d\n", buffer1.Cap())
+	p1 := make([]byte, 7)
+	n, _ := buffer1.Read(p1)
+	fmt.Printf("%d bytes were read.(call Read)", n)
+	fmt.Printf("The length of buffer :%d\n", buffer1.Len())
+	fmt.Printf("The capcity of buffer :%d\n", buffer1.Cap())
+
+	//内容泄露
+	contents = "ab"
+	buffer2 := bytes.NewBufferString(contents)
+	fmt.Printf("The capacity of new buffer with contents %q:%d\n", contents, buffer2.Cap()) //内容容器的容量为：8
+	unreadBytes := buffer2.Bytes()
+	fmt.Printf("The unread bytes of the buffer:%v\n", unreadBytes) //读取结果为[97 98]
+	buffer2.WriteString("cdefg")
+	fmt.Printf("The capacity of  buffer: %d\n", buffer2.Cap()) //底层数组不变，内容容器的容量为：8
+	// unreadBytes = unreadBytes[:cap(unreadBytes)]
+	// fmt.Printf("The unread bytes of the buffer: %v\n", unreadBytes) //读取结果为[97 98 99 100 101 102 103 0]，泄露内容
+
+	// ///更改buffer2的值
+	// unreadBytes[len(unreadBytes)-2] = byte('X') //'X'的ASCII码为88
+	// fmt.Printf("The unread bytes of the buffer: %v\n", buffer2.Bytes())
+
+	//buffer内容容器真正扩充之后就不会存在泄露问题了
+	contents = "hijklmn"
+	buffer2.WriteString(contents)
+	fmt.Printf("The capacity of  buffer: %d\n", buffer2.Cap()) //扩容了内容容器
+	///更改buffer2的值
+	unreadBytes = unreadBytes[:cap(unreadBytes)] //'X'的ASCII码为8
+	unreadBytes[len(unreadBytes)-2] = byte('X')  //'X'的ASCII码为8
+	fmt.Printf("The unread bytes of the buffer: %v\n", unreadBytes)
+	fmt.Printf("The unread bytes of the buffer: %v\n", buffer2.Bytes())
+}
--- main/demo.go
+++ main/demo.go
@@ -5,33 +5,43 @@ import (
 	"time"
 )
 
-func main() {
-	// var l list.List
-	// l.PushBack(1)
-	// l.PushBack(2)
-	// l.PushBack(3)
-	// l.PushBack(4)
-	// l.PushBack(5)
-	// element := l.Front()
-	// for i := 0; i <= l.Len(); i++ {
-	// 	fmt.Println(i, l)
-	// }
-	// lenth := l.Len()
-	// fmt.Println(element.Value)
-	// fmt.Println(lenth)
-	// fmt.Printf("hello word")
-	//chan_test1()
-	cat := Cat{
-		name: "aaa",
-		age:  10,
-		sex:  "M",
-	}
-	cat.SetName("bbbb")
-	fmt.Println(cat)
-	cat.SetName1("eee")
-	fmt.Println(cat)
-	///添加注释测试
-}
+// func main() {
+// 	// var l list.List
+// 	// l.PushBack(1)
+// 	// l.PushBack(2)
+// 	// l.PushBack(3)
+// 	// l.PushBack(4)
+// 	// l.PushBack(5)
+// 	// element := l.Front()
+// 	// for i := 0; i <= l.Len(); i++ {
+// 	// 	fmt.Println(i, l)
+// 	// }
+// 	// lenth := l.Len()
+// 	// fmt.Println(element.Value)
+// 	// fmt.Println(lenth)
+// 	// fmt.Printf("hello word")
+// 	//chan_test1()
+// 	// cat := Cat{
+// 	// 	name: "aaa",
+// 	// 	age:  10,
+// 	// 	sex:  "M",
+// 	// }
+// 	// cat.SetName("bbbb")
+// 	// fmt.Println(cat)
+// 	// cat.SetName1("eee")
+// 	// fmt.Println(cat)
+// 	///添加注释测试
+// 	for {
+
+// 		resp, err := http.Get("https://gw-boki.starpay.jp/wxpos/version")
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+// 		fmt.Println(resp)
+
+// 	}
+
+// }
 
 func chan_test() {
 	chan1 := make(chan int, 3)
